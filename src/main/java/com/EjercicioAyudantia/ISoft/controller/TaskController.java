ee95c4b1971abefc2ab2efc3c52a2b581ad4395c
Ryenka <r.ancamilla01@ufromail.cl>
2026-05-06 10:17:26 -0400

    se agrego el get en el Taskcontroller y el sistema de filtrado en tastService

--- src/main/java/com/EjercicioAyudantia/ISoft/controller/TaskController.java
+++ src/main/java/com/EjercicioAyudantia/ISoft/controller/TaskController.java
@@ -1,10 +1,18 @@
 package com.EjercicioAyudantia.ISoft.controller;
 
-import com.EjercicioAyudantia.ISoft.model.Task;
-import com.EjercicioAyudantia.ISoft.service.TaskService;
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.EjercicioAyudantia.ISoft.model.Task;
+import com.EjercicioAyudantia.ISoft.service.TaskService;
 
 @RestController
 @RequestMapping("/tasks")
@@ -21,4 +29,16 @@ public class TaskController {
         Task createdTask = taskService.save(task);
         return new ResponseEntity<>(createdTask, HttpStatus.CREATED);
     }
+
+@GetMapping
+    public ResponseEntity<List<Task>> getTasks(
+            @RequestParam(required = false) String prioridad,
+            @RequestParam(required = false) String titulo,
+            @RequestParam(required = false) String fechaLimite) {
+        
+        // Retorna la lista filtrada o completa si no hay parámetros
+        List<Task> filteredTasks = taskService.findByFilters(prioridad, titulo, fechaLimite);
+        return new ResponseEntity<>(filteredTasks, HttpStatus.OK);
+    }
+
 }
